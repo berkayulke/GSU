0316a9dddae90d4d56bd81507f95452d6b1bb51e
Berkay <berkayulke99@gmail.com>
2019-10-13 14:17:34 +0300

    dc works with same errors

---
+++ tp2/dc/dc.c
@@ -0,0 +1,370 @@
+#include "dc.h"
+
+//tested
+Person *create_person(char *name, char *surname, char *city, char *homeNumber)
+{
+  Person *per = malloc(sizeof(Person));
+
+  per->name = malloc(sizeof(char *));
+  per->surname = malloc(sizeof(char *));
+  per->city = malloc(sizeof(char *));
+  per->homeNumber = malloc(sizeof(char *));
+
+  strcpy(per->city, city);
+  strcpy(per->name, name);
+  strcpy(per->surname, surname);
+  strcpy(per->homeNumber, homeNumber);
+
+  return per;
+}
+
+PersonList *create_empty_person_list()
+{
+  PersonList *head = malloc(sizeof(PersonList));
+  head->next = head;
+  head->person = NULL;
+  head->prev = head;
+  return head;
+}
+
+Person *copy_person(Person *source)
+{
+  Person *target = create_person(source->name, source->surname, source->city, source->homeNumber);
+  return target;
+}
+
+PersonList *add_person_before(PersonList *head, Person *new_pers)
+{
+  PersonList *new_node = create_empty_person_list();
+
+  head->prev->next = new_node;
+  new_node->prev = head->prev;
+
+  new_node->person = copy_person(new_pers);
+
+  new_node->next = head;
+  head->prev = new_node;
+
+  return new_node;
+}
+
+PersonList *add_person(PersonList *head, Person *new_pers)
+{
+  //boşsa
+  if (!head->person)
+  {
+    head->person = copy_person(new_pers);
+    head->next = head;
+    head->prev = head;
+    return head;
+  }
+  //tek elemanlıysa
+  else if (head->next == head)
+  {
+    add_person_before(head, new_pers);
+    if (strcmp(head->person->surname, new_pers->surname) >= 0)
+      return head->next;
+    else
+      return head;
+  }
+  PersonList *prev = head;
+  PersonList *cur = head->next;
+  if (strcmp(head->person->surname, new_pers->surname) >= 0)
+    return add_person_before(head, new_pers);
+
+  while (cur != head)
+  {
+    if (strcmp(cur->person->surname, new_pers->surname) >= 0)
+    {
+      add_person_before(cur, new_pers);
+      return head;
+    }
+    prev = cur;
+    cur = cur->next;
+  }
+  add_person_before(head, new_pers);
+  return head;
+}
+
+PersonList *delete_person(PersonList *head, char *surname)
+{
+  PersonList *cur = head->next;
+  PersonList *prev = head;
+  PersonList *last = head->prev;
+  if (strcmp(head->person->surname, surname) == 0)
+  {
+    if (head->next == head)
+    {
+      free(head);
+      return create_empty_person_list();
+    }
+    last->next = head->next;
+    head->prev = last;
+    PersonList *for_ret = head->next;
+    free(head);
+    return for_ret;
+  }
+
+  while (cur != head)
+  {
+    if (strcmp(cur->person->surname, surname) == 0)
+    {
+      prev->next = cur->next;
+      cur->prev = prev;
+      free(cur);
+      return head;
+    }
+    cur = cur->next;
+  }
+  printf("Couldn't find it\n");
+  return head;
+}
+
+void print_person(Person *person, CityList *city_list)
+{
+  City *city = NULL;
+  //city = search_city(city_list, person->city);
+  printf("Name:       %s\n", person->name);
+  printf("Surname:    %s\n", person->surname);
+  printf("City:       %s\n", person->city);
+  if (city)
+  {
+    printf("Home Number: (%i) %s\n", city->code, person->homeNumber);
+  }
+  else
+  {
+    printf("Home Number: %s\n", person->homeNumber);
+  }
+
+  printf("-------------------\n");
+}
+
+void print_person_list(PersonList *head, CityList *city_list)
+{
+  printf("-------------------\n");
+  PersonList *cur = head;
+  PersonList *last = head->prev;
+
+  while (cur->person)
+  {
+    if (cur == last)
+      break;
+    print_person(cur->person, city_list);
+    cur = cur->next;
+  }
+  if (cur->person)
+    print_person(cur->person, city_list);
+}
+
+void search_person(PersonList *head, char *surname, CityList *city_list)
+{
+  PersonList *cur = head;
+  PersonList *last = head->prev;
+
+  while (cur)
+  {
+    if (cur == last)
+      break;
+    if (strcmp(cur->person->surname, surname) == 0)
+    {
+      printf("-------------------\n");
+      printf("person is: \n");
+      print_person(cur->person, city_list);
+      return;
+    }
+    cur = cur->next;
+  }
+  if (strcmp(cur->person->surname, surname) == 0)
+  {
+    printf("-------------------\n");
+    printf("person is: \n");
+    print_person(cur->person, city_list);
+    return;
+  }
+  printf("Couldn't find it \n");
+}
+
+CityList *create_empty_city_list()
+{
+  CityList *head = malloc(sizeof(CityList));
+  head->next = head;
+  head->city = NULL;
+  head->prev = head;
+  return head;
+}
+
+City *create_city(char *name, int code)
+{
+  City *new_city = malloc(sizeof(City));
+  new_city->code = code;
+  new_city->name = malloc(sizeof(char *));
+  strcpy(new_city->name, name);
+  return new_city;
+}
+
+City *copy_city(City *source)
+{
+  City *new_city = create_city(source->name, source->code);
+  return new_city;
+}
+
+CityList *add_city_before(CityList *head, City *new_city)
+{
+  CityList *new_node = create_empty_city_list();
+  head->prev->next = new_node;
+  new_node->prev = head->prev;
+  new_node->city = copy_city(new_city);
+  new_node->next = head;
+  head->prev = new_node;
+  return new_node;
+}
+
+CityList *add_city(CityList *head, City *new_city)
+{
+  if (!head->city)
+  {
+    head->city = copy_city(new_city);
+    head->next = head;
+    head->prev = head;
+    return head;
+  }
+  //tek elemanlıysa
+  else if (head->next == head)
+  {
+    add_city_before(head, new_city);
+    if (strcmp(head->city->name, new_city->name) >= 0)
+      return head->next;
+    else
+      return head;
+  }
+  CityList *prev = head;
+  CityList *cur = head->next;
+
+  if (strcmp(head->city->name, new_city->name) >= 0)
+    return add_city_before(head, new_city);
+
+  while (cur != head)
+  {
+    if (strcmp(cur->city->name, new_city->name) >= 0)
+    {
+      add_city_before(cur, new_city);
+      return head;
+    }
+    prev = cur;
+    cur = cur->next;
+  }
+  add_city_before(head, new_city);
+  return head;
+}
+
+City *search_city(CityList *head, char *name)
+{
+  if (!head || !name)
+    return NULL;
+  CityList *cur = head;
+  CityList *last = head->prev;
+
+  while (cur)
+  {
+    if (cur == last)
+      break;
+    if (strcmp(cur->city->name, name) == 0)
+      return cur->city;
+
+    cur = cur->next;
+  }
+  if (strcmp(cur->city->name, name) == 0)
+    return cur->city;
+  return NULL;
+}
+
+CityList *delete_city(CityList *head, char *name)
+{
+  CityList *cur = head->next;
+  CityList *prev = head;
+  CityList *last = head->prev;
+
+  if (strcmp(head->city->name, name) == 0)
+  {
+    if (head->next == head)
+    {
+      free(head);
+      return create_empty_city_list();
+    }
+    last->next = head->next;
+    head->next->prev = last;
+    CityList *for_ret = head->next;
+    free(head);
+    return for_ret;
+  }
+
+  while (cur != head)
+  {
+    if (strcmp(cur->city->name, name) == 0)
+    {
+      prev->next = cur->next;
+      cur->next->prev = prev;
+      free(cur);
+      return head;
+    }
+    cur = cur->next;
+  }
+  printf("Couldn't find it\n");
+  return head;
+}
+void print_city(City *city)
+{
+  if (city)
+  {
+    printf("City Name:    %s\n", city->name);
+    printf("City Code:    %i\n", city->code);
+    printf("-------------------\n");
+  }
+  else
+  {
+    printf("Couldn't find it\n");
+  }
+}
+void print_city_list(CityList *head)
+{
+  printf("-------------------\n");
+  CityList *cur = head;
+  CityList *last = head->prev;
+
+  while (cur->city)
+  {
+    if (cur == last)
+      break;
+    print_city(cur->city);
+    cur = cur->next;
+  }
+  if (cur->city)
+    print_city(cur->city);
+}
+
+void get_person(PersonList **head)
+{
+  char name[128], surname[128], city[128], home_number[128];
+  printf("Name:\n ");
+  scanf("%s", &name);
+  printf("Surame:\n ");
+  scanf("%s", &surname);
+  printf("City:\n ");
+  scanf("%s", &city);
+  printf("Home Number:\n ");
+  scanf("%s", &home_number);
+  Person *new_pers = create_person(name, surname, city, home_number);
+  *head = add_person(*head, new_pers);
+}
+
+void get_city(CityList **head)
+{
+  char name[128];
+  int code;
+  printf("Name:\n ");
+  scanf("%s", &name);
+  printf("Code:\n ");
+  scanf("%i", &code);
+  City *new_city = create_city(name, code);
+  *head = add_city(*head, new_city);
+}
---
+++ tp2/dc/dc.h
@@ -0,0 +1,61 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef __DC_H_
+#define __DC_H_
+
+typedef struct city
+{
+  char *name;
+  int code;
+} City;
+
+typedef struct person
+{
+  char *name;
+  char *surname;
+  char *city;
+  char *homeNumber;
+} Person;
+
+typedef struct PersonList
+{
+  Person *person;
+  struct PersonList *next;
+  struct PersonList *prev;
+} PersonList;
+
+typedef struct CityList
+{
+  City *city;
+  struct CityList *next;
+  struct CityList *prev;
+} CityList;
+
+//For person list
+PersonList *create_empty_person_list();
+Person *create_person(char *name, char *surname, char *city, char *homeNumber);
+Person *copy_person(Person *source);
+PersonList *add_person(PersonList *head, Person *new_pers);
+void search_person(PersonList *head, char *surname, CityList *city_list);
+PersonList *delete_person(PersonList *head, char *surname);
+void print_person(Person *person, CityList *city_list);
+void print_person_list(PersonList *head, CityList *city_list);
+PersonList *add_person_before(PersonList *head, Person *new_pers);
+
+//For city list
+CityList *create_empty_city_list();
+City *create_city(char *name, int code);
+City *copy_city(City *source);
+CityList *add_city(CityList *head, City *new_city);
+City *search_city(CityList *head, char *name);
+CityList *delete_city(CityList *head, char *name);
+void print_city(City *city);
+void print_city_list(CityList *head);
+
+//For menu
+void get_person(PersonList **head);
+void get_city(CityList **head);
+
+#endif
