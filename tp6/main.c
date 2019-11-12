38401a38b6ed1343c64b49d9699b79a72373cb70
berkayulke <berkayulke99@gmail.com>
2019-11-12 11:27:11 +0300

    fixed get distances

--- tp6/main.c
+++ tp6/main.c
@@ -10,13 +10,15 @@ void print_s(Sensor s){
 float dist(float x1,float y1){
     return 10.0/3.0; 
 }
-
+int i,j;
 int main(){
     char file_name[] = "sensor_locations.csv";
     Sensor* sensors; 
     sensors = get_sensor(file_name);
-    //print_sensors(sensors);
     float**ar=get_distances(sensors);
-
+    print_sensors(sensors);
+    for(i=0;i < 70; ++i)
+        for(j=0;j<70;++j)
+            printf("(%i,%i)->%f\n",i,j,ar[i][j]);
     return 0;
 }
